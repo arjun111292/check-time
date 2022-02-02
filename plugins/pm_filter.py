d81a877d8cafbafa40f7d46c1e434842db86f08a
T E L E Z E N <94111048+TELEZEN@users.noreply.github.com>
2022-02-02 09:05:59 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -546,7 +546,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('Rᴇғʀᴇsʜ', callback_data='rfrsh')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        total = await Media.count_documents()
+        total = ('1988325')
         users = await db.total_users_count()
         chats = await db.total_chat_count()
         monsize = await db.get_db_size()
