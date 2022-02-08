0f11038766ebd3c68b5edf2bf97dc0c1cc642878
T E L E Z E N <94111048+TELEZEN@users.noreply.github.com>
2022-02-08 12:45:11 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/TELEZEN/jennie-multiple-pic-test.git /jennie-multiple-pic-test
+  git clone https://github.com/TELEZEN/Jennie-tr-real-test.git /Jennie-tr-real-test
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /jennie-multiple-pic-test
+  git clone $UPSTREAM_REPO /Jennie-tr-real-test
 fi
-cd /jennie-multiple-pic-test
+cd /Jennie-tr-real-test
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
