af2e87a44436addece8a21c562fc6072c33958f3
T E L E Z E N <94111048+TELEZEN@users.noreply.github.com>
2022-02-11 08:14:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -41,6 +41,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/+eNWA_Kp7IIpiNzll')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'TeamEvamaria')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
