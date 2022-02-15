9fddc7b99f70f9256012339e184f7ad6dc25104c
T E L E Z E N <94111048+TELEZEN@users.noreply.github.com>
2022-02-15 15:29:46 +0530

    Update Whois.py

--- plugins/Who is this/Whois.py
+++ plugins/Who is this/Whois.py
@@ -9,9 +9,9 @@ from datetime import datetime
 from pyrogram import Client, filters
 from pyrogram.errors import UserNotParticipant
 from info import COMMAND_HAND_LER
-from plugins.Extract.extract_user import extract_user
+from plugins.extract.extract_user import extract_user
 from plugins.neededshits.cust_p_filters import f_onw_fliter
-from plugins.Online_Ammaven.last_online_hlpr.py import last_online
+from plugins.online_Ammaven.last_online_hlpr.py import last_online
 
 
 @Client.on_message(
