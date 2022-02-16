2975d1111e7b27320fc0b09bb6eea18182895a8f
T E L E Z E N <94111048+TELEZEN@users.noreply.github.com>
2022-02-16 12:34:17 +0530

    Update breeak_up.py

--- plugins/t_4_pgs/breeak_up.py
+++ plugins/t_4_pgs/breeak_up.py
@@ -1,24 +1,28 @@
+#created by by @Arjun_La_Lis_A - tg
+#use with proper credits
+
+import random
 from pyrogram import Client, filters
 from info import COMMAND_HAND_LER
 from plugins.neededshits.cust_p_filters import f_onw_fliter
 
 
+PASSWORD_STRINGS = ("😂😂😂",
+                    "😂😂😂🤣",
+                   )
+PICS1_PC = ("https://telegra.ph/file/6d5a7465f6ae6c2f6fb6b.jpg")
 
-# EMOJI CONSTANTS
-BREAK_YOUR_LOVE = "💔"
-# EMOJI CONSTANTS
 
 @Client.on_message(
-    filters.command(["break", "thepp"])
+    filters.command("/breakup", COMMAND_HAND_LER) &
+    f_onw_fliter
 )
-async def break_thepp(client, message):
-    """ /break an @animatedbreak """
-    rep_mesg_id = message.message_id
+async def breakup(_, message):
+    """ /breakup strings """
+    effective_string = random.choice(PASSWORD_STRINGS)
     if message.reply_to_message:
-        rep_mesg_id = message.reply_to_message.message_id
-    await client.send_dice(
-        chat_id=message.chat.id,
-        emoji=BREAK_YOUR_LOVE,
-        disable_notification=True,
-        reply_to_message_id=rep_mesg_id
-    )
+        await message.reply_photo(
+            photo=(PICS1_PC),
+            message.reply_to_message.reply_text(effective_string)
+    else:
+        await message.reply_text(effective_string)
