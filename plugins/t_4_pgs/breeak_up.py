1498856282a04e7da3d2e99ffa4b5c2b7250f1ee
T E L E Z E N <94111048+TELEZEN@users.noreply.github.com>
2022-02-16 12:55:33 +0530

    Update breeak_up.py

--- plugins/t_4_pgs/breeak_up.py
+++ plugins/t_4_pgs/breeak_up.py
@@ -7,10 +7,10 @@ from info import COMMAND_HAND_LER
 from plugins.neededshits.cust_p_filters import f_onw_fliter
 
 
-PASSWORD_STRINGS = ("😂😂😂",
-                    "😂😂😂🤣",
-                   )
-PICS1_PC = ("https://telegra.ph/file/6d5a7465f6ae6c2f6fb6b.jpg")
+PASSWORD_STRINGS = (
+    "Habeebi.. Come to dubai 😂😂 ",
+    "Habeebi.. Come to dubai 😂😂",
+)
 
 
 @Client.on_message(
@@ -21,11 +21,6 @@ async def breakup(_, message):
     """ /breakup strings """
     effective_string = random.choice(PASSWORD_STRINGS)
     if message.reply_to_message:
-        await message.reply_to_message.reply_photo(
-            photo=(PICS1_PC),
-            message.reply_to_message.reply_text(effective_string)
+        await message.reply_to_message.reply_text(effective_string)
     else:
-       message.reply_to_message:
-        await message.reply_to_message.reply_photo(
-            photo=(PICS1_PC),
-            message.reply_to_message.reply_text(effective_string)
+        await message.reply_text(effective_string)
