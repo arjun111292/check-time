1d6291974784c97eb990989d8c447a5b3f0956f0
T E L E Z E N <94111048+TELEZEN@users.noreply.github.com>
2022-02-16 09:57:32 +0530

    Update song_video.py

--- plugins/song_video.py
+++ plugins/song_video.py
@@ -69,7 +69,7 @@ def a(client, message: Message):
             #     m.edit("Exceeded 30mins cap")
             #     return
 
-            performer = f"[Zaute Km]" 
+            performer = f"[🇦🇷🇯🇺🇳]" 
             thumb_name = f'thumb{message.message_id}.jpg'
             thumb = requests.get(thumbnail, allow_redirects=True)
             open(thumb_name, 'wb').write(thumb.content)
