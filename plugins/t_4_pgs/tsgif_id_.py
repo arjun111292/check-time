85ba35a4eb83da1ce5afbc2596cdf73d2dd0c073
T E L E Z E N <94111048+TELEZEN@users.noreply.github.com>
2022-02-17 13:40:27 +0530

    Delete tsgif_id_.py

--- plugins/t_4_pgs/tsgif_id_.py
+++
@@ -1,9 +0,0 @@
-from pyrogram import Client, filters
-
-# Gif ID 
-@Client.on_message(filters.command(["gifid"]))
-async def gifid(bot, message):   
-    if message.reply_to_message.gif:
-       await message.reply(f"**Gif ID is**  \n `{message.reply_to_message.gif.file_id}` \n \n ** Unique ID is ** \n\n`{message.reply_to_message.gif.file_unique_id}`", quote=True)
-    else: 
-       await message.reply("Not a Gif file🥱")
