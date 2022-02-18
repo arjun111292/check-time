6108a81249a86673abda6ef08caee8ad83b8e1f0
T E L E Z E N <94111048+TELEZEN@users.noreply.github.com>
2022-02-18 12:52:12 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/TELEZEN/filter-main.git /filter-main
+  git clone https://github.com/TELEZEN/PIC_TESTER.git /PIC_TESTER
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /filter-main
+  git clone $UPSTREAM_REPO /PIC_TESTER
 fi
-cd /filter-main
+cd /PIC_TESTER
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
