cce5d6bf5904cb2d7d5e9c6417bf7d983d466b24
T E L E Z E N <94111048+TELEZEN@users.noreply.github.com>
2022-02-18 12:54:57 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -457,10 +457,10 @@ async def cb_handler(client: Client, query: CallbackQuery):
             ],[
             InlineKeyboardButton('ʜᴏᴍᴇ', callback_data='start'),
         ]]
-        md = InputMediaPhoto(photo = "indilla",
+        md = InputMediaPhoto(media = "picture/indilla.jpg",
             caption=script.FLTR_TXT.format(query.from_user.mention),
             parse_mode='html')
-        await query.message.edit_photo(photo = md,
+        await query.message.edit_media(media = md,
             reply_markup = reply_markup
         )
     elif query.data == "help":
