89daf88af636e1b9e341b7924dcd337a00e1a665
T E L E Z E N <94111048+TELEZEN@users.noreply.github.com>
2022-02-19 22:21:58 +0530

    Create jenlisa.py

--- picture/jenniekutty/jenlisa.py
+++ picture/jenniekutty/jenlisa.py
@@ -1,1 +1,13 @@
+#64K Rsl images - may come across download - upload inqualitys...
+#By Arjun Pradeep - TG BOT = CT_NE_BOT , Developer { Arjun }
 JENNIE1 = ("https://telegra.ph/file/e5995975e1b2b563b078c.jpg")
+
+JENNIE2 = ("https://telegra.ph/file/e5995975e1b2b563b078c.jpg")
+
+JENNIE3 = ("https://telegra.ph/file/e5995975e1b2b563b078c.jpg")
+
+JENNIE4 = ("https://telegra.ph/file/e5995975e1b2b563b078c.jpg")
+
+JENNIE5 = ("https://telegra.ph/file/e5995975e1b2b563b078c.jpg")
+
+JENNIE6 = ("https://telegra.ph/file/e5995975e1b2b563b078c.jpg")
