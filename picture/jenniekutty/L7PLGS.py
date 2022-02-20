f84d9238d9cfb8a4d0ca09018c6e364bd9ae64b2
T E L E Z E N <94111048+TELEZEN@users.noreply.github.com>
2022-02-20 21:26:13 +0530

    Update L7PLGS.py

--- picture/jenniekutty/L7PLGS.py
+++ picture/jenniekutty/L7PLGS.py
@@ -1,7 +1,6 @@
-JENPIC1 = ("https://telegra.ph/file/6daea6881ec12032639cc.jpg")
-JENPIC1 = ("https://telegra.ph/file/0c2b9ac87daff480bef7d.jpg")
-JENPIC1 = ("https://telegra.ph/file/868db0e105e59a7dce604.jpg")
-JENPIC1 = ("https://telegra.ph/file/b7aa8612487f6ab1b6c5f.jpg")
-JENPIC1 = ("https://telegra.ph/file/1e0124e664fa978f106e4.jpg")
-JENPIC1 = ("https://telegra.ph/file/73c65cb38bc0930d1531a.jpg")
-JENPIC1 = ("https://telegra.ph/file/94d2eeffee6631ae49a98.jpg")
+JENMULPCTST = (environ.get('JENMULPCTST', 'https://telegra.ph/file/6daea6881ec12032639cc.jpg https://telegra.ph/file/0c2b9ac87daff480bef7d.jpg https://telegra.ph/file/868db0e105e59a7dce604.jpg')).split()
+
+JENPIC4 = ("https://telegra.ph/file/b7aa8612487f6ab1b6c5f.jpg")
+JENPIC5 = ("https://telegra.ph/file/1e0124e664fa978f106e4.jpg")
+JENPIC6 = ("https://telegra.ph/file/73c65cb38bc0930d1531a.jpg")
+JENPIC7 = ("https://telegra.ph/file/94d2eeffee6631ae49a98.jpg")
