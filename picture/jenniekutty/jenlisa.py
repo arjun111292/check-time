58463c1cf4069a0d017e920167ff240930a56387
T E L E Z E N <94111048+TELEZEN@users.noreply.github.com>
2022-02-20 09:26:02 +0530

    Update jenlisa.py

--- picture/jenniekutty/jenlisa.py
+++ picture/jenniekutty/jenlisa.py
@@ -2,12 +2,12 @@
 #By Arjun Pradeep - TG BOT = CT_NE_BOT , Developer { Arjun }
 JENNIE1 = ("https://telegra.ph/file/e5995975e1b2b563b078c.jpg")
 
-JENNIE2 = ("https://telegra.ph/file/e5995975e1b2b563b078c.jpg")
+JENNIE2 = ("https://telegra.ph/file/625dc7380331f90d04f47.jpg")
 
-JENNIE3 = ("https://telegra.ph/file/e5995975e1b2b563b078c.jpg")
+JENNIE3 = ("https://telegra.ph/file/f6180b3fea90c95cd4a94.jpg")
 
-JENNIE4 = ("https://telegra.ph/file/e5995975e1b2b563b078c.jpg")
+JENNIE4 = ("https://telegra.ph/file/b8ad33c650fd0f4d8e776.jpg")
 
-JENNIE5 = ("https://telegra.ph/file/e5995975e1b2b563b078c.jpg")
+JENNIE5 = ("https://telegra.ph/file/e855727494d26b8b0618c.jpg")
 
-JENNIE6 = ("https://telegra.ph/file/e5995975e1b2b563b078c.jpg")
+JENNIE6 = ("https://telegra.ph/file/0968e1f0f35df8e43597c.jpg")
