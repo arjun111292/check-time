ddde09bf87c08002aae89132a4b06af81b875353
T E L E Z E N <94111048+TELEZEN@users.noreply.github.com>
2022-02-20 21:21:23 +0530

    Create L7PLGS.py

---
+++ picture/jenniekutty/L7PLGS.py
@@ -0,0 +1,7 @@
+JENPIC1 = ("https://telegra.ph/file/6daea6881ec12032639cc.jpg")
+JENPIC1 = ("https://telegra.ph/file/0c2b9ac87daff480bef7d.jpg")
+JENPIC1 = ("https://telegra.ph/file/868db0e105e59a7dce604.jpg")
+JENPIC1 = ("https://telegra.ph/file/b7aa8612487f6ab1b6c5f.jpg")
+JENPIC1 = ("https://telegra.ph/file/1e0124e664fa978f106e4.jpg")
+JENPIC1 = ("https://telegra.ph/file/73c65cb38bc0930d1531a.jpg")
+JENPIC1 = ("https://telegra.ph/file/94d2eeffee6631ae49a98.jpg")
