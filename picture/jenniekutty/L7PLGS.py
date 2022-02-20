2e70f18fabd2e69654224a23da25e101bf2c7771
T E L E Z E N <94111048+TELEZEN@users.noreply.github.com>
2022-02-20 21:44:42 +0530

    Update L7PLGS.py

--- picture/jenniekutty/L7PLGS.py
+++ picture/jenniekutty/L7PLGS.py
@@ -1,4 +1,6 @@
-JENMULPCTST = ('https://telegra.ph/file/6daea6881ec12032639cc.jpg https://telegra.ph/file/0c2b9ac87daff480bef7d.jpg https://telegra.ph/file/868db0e105e59a7dce604.jpg')
+JENMULPCTST = ('https://telegra.ph/file/6daea6881ec12032639cc.jpg',
+               'https://telegra.ph/file/0c2b9ac87daff480bef7d.jpg',
+               'https://telegra.ph/file/868db0e105e59a7dce604.jpg')
 
 JENPIC4 = ("https://telegra.ph/file/b7aa8612487f6ab1b6c5f.jpg")
 JENPIC5 = ("https://telegra.ph/file/1e0124e664fa978f106e4.jpg")
