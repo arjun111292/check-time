6b8c6b09111eb37442afac28381ad45bd99efb7c
T E L E Z E N <94111048+TELEZEN@users.noreply.github.com>
2022-02-21 16:11:33 +0530

    Update L7PLGS.py

--- picture/jenniekutty/L7PLGS.py
+++ picture/jenniekutty/L7PLGS.py
@@ -5,17 +5,17 @@ JENMULPCTST = ("https://telegra.ph/file/6daea6881ec12032639cc.jpg",
                "https://telegra.ph/file/1e0124e664fa978f106e4.jpg",
                "https://telegra.ph/file/73c65cb38bc0930d1531a.jpg",
                "https://telegra.ph/file/94d2eeffee6631ae49a98.jpg",
-               https://telegra.ph/file/1c5c3e87f7a0a8fa35ad0.jpg
-https://telegra.ph/file/047923cf6405cb026e60e.jpg
-https://telegra.ph/file/e01d54ff4160b11c6386b.jpg
-https://telegra.ph/file/db3b6c59f18a648dcf17e.jpg
-https://telegra.ph/file/f3218d7951730cee72be5.jpg
-https://telegra.ph/file/7987906ac76df37356ea5.jpg
-https://telegra.ph/file/be1d9a9693fced3b28753.jpg
-https://telegra.ph/file/732b68393756cc73573cb.jpg
-https://telegra.ph/file/f85ffcaeea2597ac0605d.jpg
-https://telegra.ph/file/acf5fd86b20dbcd757773.jpg
-https://telegra.ph/file/3fe9e7cdba499946fb563.jpg
-https://telegra.ph/file/92d238163e7be896c7bb9.jpg
-https://telegra.ph/file/a90815e245f30c50f2a7b.jpg
-https://telegra.ph/file/2ac16c4fcc17960deced8.jpg
+               "https://telegra.ph/file/1c5c3e87f7a0a8fa35ad0.jpg",
+               "https://telegra.ph/file/047923cf6405cb026e60e.jpg",
+               "https://telegra.ph/file/e01d54ff4160b11c6386b.jpg",
+               "https://telegra.ph/file/db3b6c59f18a648dcf17e.jpg",
+               "https://telegra.ph/file/f3218d7951730cee72be5.jpg",
+               "https://telegra.ph/file/7987906ac76df37356ea5.jpg",
+               "https://telegra.ph/file/be1d9a9693fced3b28753.jpg",
+               "https://telegra.ph/file/732b68393756cc73573cb.jpg",
+               "https://telegra.ph/file/f85ffcaeea2597ac0605d.jpg",
+               "https://telegra.ph/file/acf5fd86b20dbcd757773.jpg",
+               "https://telegra.ph/file/3fe9e7cdba499946fb563.jpg",
+               "https://telegra.ph/file/92d238163e7be896c7bb9.jpg",
+               "https://telegra.ph/file/a90815e245f30c50f2a7b.jpg",
+               "https://telegra.ph/file/2ac16c4fcc17960deced8.jpg")
