ad89d7b6f9d0b9e3b99082731d6f8a877756503f
shamil_lisa <97886263+shamil-lol@users.noreply.github.com>
2022-03-07 16:23:05 +0530

    Update password.py

--- plugins/t_4_pgs/password.py
+++ plugins/t_4_pgs/password.py
@@ -1,18 +1,30 @@
 #created by by @Arjun_La_Lis_A - tg
 #use with proper credits
 
+import string
 import random
 from pyrogram import Client, filters
 from info import COMMAND_HAND_LER
 from plugins.neededshits.cust_p_filters import f_onw_fliter
 
-
+"""
 PASSWORD_STRINGS = (
     "പാസ്‌വേഡ് ഉണ്ടാക്കാൻ എനിക്കറിയില്ല🙄",
     "പാസ്‌വേഡ് ഉണ്ടാക്കാൻ എനിക്കറിയില്ല🙄..",
     "പാസ്‌വേഡ് ഉണ്ടാക്കാൻ എനിക്കറിയില്ല😫", 
 )
+"""
 
+characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
+
+def passw (leng):
+  length = int(leng)
+  random.shuffle(characters)
+  pas = []
+  for i in range (length):
+    pas.append(random.choice(characters))
+  random.shuffle(pas)
+  return "".join(pas)
 
 @Client.on_message(
     filters.command("password", COMMAND_HAND_LER) &
@@ -20,7 +32,7 @@ PASSWORD_STRINGS = (
 )
 async def password(_, message):
     """ /password strings """
-    effective_string = random.choice(PASSWORD_STRINGS)
+    effective_string = passw(20)
     if message.reply_to_message:
         await message.reply_to_message.reply_text(effective_string)
     else:
