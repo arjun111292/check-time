c121b16435d9774b5ee8638edc74fb4481fb6945
ARJUN 369 X <94111048+arjun111292@users.noreply.github.com>
2022-03-07 11:12:32 +0530

    Update callsmusic.py

--- plugins/vc/end/callsmusic.py
+++ plugins/vc/end/callsmusic.py
@@ -4,7 +4,7 @@ from pytgcalls import PyTgCalls
 from pytgcalls.types import Update
 from pytgcalls.types.input_stream import InputAudioStream
 from pytgcalls.types.input_stream import InputStream
-from config import API_HASH, API_ID, SESSION_NAME
+from info import API_HASH, API_ID, SESSION_NAME
 
 
 client = Client(SESSION_NAME, API_ID, API_HASH)
