43acc091cc82b5bdbcef12a024f616bbb25d2345
shamil_lisa <97886263+shamil-lol@users.noreply.github.com>
2022-03-07 19:39:52 +0530

    Update phone_number.py

--- plugins/t_4_pgs/phone_number.py
+++ plugins/t_4_pgs/phone_number.py
@@ -1,2 +1,11 @@
 # developing
+from pyrogram import Client, filters
+from info import COMMAND_HAND_LER
+from plugins.neededshits.cust_p_filters import f_onw_fliter
 from phonenumbers import carrier, geocoder, timezone
+
+def fetch_ph (ph):
+  ph = phonenumbers.parse(ph)
+  return carrier.name_for_number(ph, "en")
+  return geocoder.description_for_number(ph, "en")
+  return timezone.time_zones_for_number(ph)
