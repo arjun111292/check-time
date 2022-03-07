908ffe38000920abd71cd90acfa9e7d45aae3f65
ARJUN 369 X <94111048+arjun111292@users.noreply.github.com>
2022-03-07 13:24:21 +0530

    Update youtube.py

--- plugins/vc/reciever/youtube.py
+++ plugins/vc/reciever/youtube.py
@@ -2,7 +2,7 @@ from os import path
 
 from yt_dlp import YoutubeDL
 
-from config import DURATION_LIMIT
+from konfig import DURATION_LIMIT
 from helpers.errors import DurationLimitError
 
 ydl_opts = {
