6f1c0fd4d836cf924730676a9dafd95e5f803f95
ARJUN 369 X <94111048+arjun111292@users.noreply.github.com>
2022-03-11 13:19:09 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -495,7 +495,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "about":
         buttons= [[
             InlineKeyboardButton('му fιℓєѕ', url='https://t.me/+f2zfFt4QnHszYjll'),
-            InlineKeyboardButton('ѕσυя¢є', show_alert='Nokki irunno, ippam kittum 😂')
+            InlineKeyboardButton('ѕσυя¢є', text='Nokki irunno, ippam kittum 😂', show_alert= True)
             ],[
             InlineKeyboardButton('нєℓρ', callback_data='help'),
             InlineKeyboardButton('∂єνєℓσρєя', url='https://t.me/Username_Not_Found_404_Error')
